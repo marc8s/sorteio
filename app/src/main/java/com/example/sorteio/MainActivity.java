9ff2f04c46f7bea6023fa8742e507621f404f252
Marc8s <marcus.ecomp@gmail.com>
2021-03-16 12:13:10 +0000

    lógica e ajustes no layout

--- app/src/main/java/com/example/sorteio/MainActivity.java
+++ app/src/main/java/com/example/sorteio/MainActivity.java
@@ -3,6 +3,10 @@ package com.example.sorteio;
 import androidx.appcompat.app.AppCompatActivity;
 
 import android.os.Bundle;
+import android.view.View;
+import android.widget.TextView;
+
+import java.util.Random;
 
 public class MainActivity extends AppCompatActivity {
 
@@ -10,6 +14,12 @@ public class MainActivity extends AppCompatActivity {
     protected void onCreate(Bundle savedInstanceState) {
         super.onCreate(savedInstanceState);
         setContentView(R.layout.activity_main);
+    }
+    public void sortearNumero(View view){
+        TextView texto = findViewById(R.id.textView_resultado);
+        int numeroSorteado = new Random().nextInt(11);
+
+        texto.setText(getString(R.string.numero_sorteado) + numeroSorteado);
     }
 
 
